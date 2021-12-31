b0819e3446ac37ba8c63b0603b8a3407cd2d06d7
Kevin Song <chips@ksong.dev>
2021-12-31 04:22:29 -0600

    Actually build stuff

--- dist/index.js
+++ dist/index.js
@@ -483,7 +483,7 @@ class BenchmarkComplete {
  */
 function resultsFromJSONString(s) {
     try {
-        const messages = s.split('\n');
+        const messages = s.trim().split('\n');
         return messages
             .map(s => JSON.parse(s))
             .filter(msg => msg.reason != null && msg.reason == 'benchmark-complete')
